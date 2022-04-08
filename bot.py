4153de5e2a1c2708eb8b6cd98207240d6778a307
heycryptobob <heycryptobob@gmail.com>
2022-04-08 17:55:30 +0200

    added heroku configs

--- bot.py
+++ bot.py
@@ -3,12 +3,8 @@ from telegram.ext import *
 from requests import *
 from os import environ
 from dotenv import load_dotenv
-load_dotenv()
 
-token = environ.get('TOKEN')
-print("Token:", token)
-updater = Updater(token=token)
-dispatcher = updater.dispatcher
+load_dotenv()
 
 buttons = [
     [
@@ -46,6 +42,7 @@ def messageHandler(update: Update, context: CallbackContext):
     for row in buttons:
         for btn in row:
             if btn[0] == update.message.text:
+                print("Button pressed: ", update.message.text)
                 with open(btn[1], "r") as file:
                     text = file.read()
                     context.bot.send_message(
@@ -59,8 +56,31 @@ def newChatMember(update: Update, context: CallbackContext):
     keyboard = buildKeyboard()
     context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to $CronoApe!", reply_markup=ReplyKeyboardMarkup(keyboard))
 
-dispatcher.add_handler(CommandHandler("cronoape", startCommand))
-dispatcher.add_handler(MessageHandler(Filters.text, messageHandler))
-dispatcher.add_handler(MessageHandler(Filters.status_update.new_chat_members, newChatMember))
 
-updater.start_polling()
+if __name__ == "__main__":
+    print("Starting up the keyboard...")
+    
+    TOKEN = environ.get('TOKEN')
+    NAME = environ.get('HEROKU_APP_NAME')
+    PORT = environ.get('PORT')
+    ENV = environ.get('PYTHON_ENV')
+
+    updater = Updater(token=TOKEN)
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("cronoape", startCommand))
+    dispatcher.add_handler(MessageHandler(Filters.text, messageHandler))
+    dispatcher.add_handler(MessageHandler(Filters.status_update.new_chat_members, newChatMember))
+
+    if ENV == "production":
+        updater.start_webhook(
+            listen="0.0.0.0",
+            port=int(PORT),
+            url_path=TOKEN,
+            webhook_url=f"https://{NAME}.herokuapp.com/{TOKEN}"
+        )
+        updater.idle()
+    else:
+        updater.start_polling()
+
+    print("Keyboard is operational.")
